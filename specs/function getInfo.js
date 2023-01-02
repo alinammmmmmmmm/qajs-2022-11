7954ee2d1e6ff8a974ad9361e8b20558771405a6
alina <alinkina-pochaa@mail.ru>
2023-01-03 00:04:03 +0300

    objects

--- specs/function getInfo.js
+++ specs/function getInfo.js
@@ -1,20 +1,29 @@
 function getInfo (name,surname,age) {
     return `Имя: ${name},Фамилия: ${surname},возраст: ${age}`
 }
-
 const myName = getInfo (Alina,Les,11);
 console.log(myName);
 
+function returnObject (оbject) {
+return [returnObject.name, returnObject.surname, returnObject.age]
+}
+const myObject = returnObject (Alina,Les,11);
+console.log(myObject);
+
+function getInfo (name,surname,age) {
+    return `Имя: ${name},Фамилия: ${surname},возраст: ${age}`
+}
+const myCreds = getInfo (Alina,Les,11);
+console.log(myCreds);
+
+
+function returnObject (оbject) {
+return [returnObject.name, returnObject.surname, returnObject.age]
+}
 const оbject = {
     name:'Alina',
     surname:'Malina',
     age:1,
 }
-
-function returnObject (оbject) {
-return [returnObject.name, returnObject.surname, returnObject.age]
-}
-
-
-const myObject = returnObject (Alina,Les,11);
-console.log(myObject);
+const myyObject = returnObject (Alina,Les,11);
+console.log(myyObject);
